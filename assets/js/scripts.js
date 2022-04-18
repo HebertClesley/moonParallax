2c4befa6e3824b9251282ae49d85669589a43483
Hebert Monteiro <monteiro.hebert@gmail.com>
2022-04-18 16:49:59 -0300

    projeto finalizado

---
+++ assets/js/scripts.js
@@ -0,0 +1,16 @@
+let bg = document.getElementById("bg");
+let moon = document.getElementById("moon");
+let mountain = document.getElementById("mountain");
+let road = document.getElementById("road");
+let text = document.getElementById("text");
+
+window.addEventListener("scroll", function () {
+  // 'Value' recebe o valor apartir so top e calcula em 'px' e modificar os valores "top, left de position: absolute; causando animação"
+  var value = window.scrollY;
+
+  bg.style.top = value * 0.5 + "px";
+  moon.style.left = -value * 0.5 + "px";
+  mountain.style.top = -value * 0.15 + "px";
+  road.style.top = value * 0.15 + "px";
+  text.style.top = value * 1 + "px";
+});
